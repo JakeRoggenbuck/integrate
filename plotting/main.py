b898112233b24957d6b4252cd37f0c3dc8d8daf7
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-04-17 11:28:36 -0700

    Add final project
    - main.c to calculate derivative
    - main.py to view data

---
+++ plotting/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/out.csv")
+
+x = df["x"]
+y = df["y"]
+yprime = df["y'"]
+
+plt.plot(x, y, color='r', label='y')
+plt.plot(x, yprime, color='g', label='yprime')
+plt.show()
+
+print(df)
---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+#define SIZE 1000
+#define H 0.001
+
+void derive(double (*func)(double)) {
+    printf("x,y,y'\n");
+    for (int i = -SIZE; i < SIZE; i++) {
+        double x = i * H;
+        double k = (func((double)x + H) - func(x)) / H;
+        double y = func(x);
+        printf("%f,%f,%f\n", x, y, k);
+    }
+}
+
+double function(double x) { return (pow(x, 3) * 4) + 2; }
+
+int main() {
+
+    derive(function);
+    return 0;
+}
